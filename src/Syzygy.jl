e38f089da573200c7bd2e2c037eba51a6e6d9fd1
Caspar Bruenech <c.w.bruenech@pm.me>
2023-05-10 13:57:25 +0200

    added precompilation

--- src/Syzygy.jl
+++ src/Syzygy.jl
@@ -2,6 +2,8 @@
 
 module Syzygy
 
+    using PrecompileTools
+
     include("units.jl")
     include("constants.jl")
     
@@ -26,4 +28,15 @@ module Syzygy
     export simulation, simulate
     export analyse_simulation
     export 𝒢, pI, bI, ParticleIndex, BinaryIndex
+
+    @compile_workload begin
+        triple = multibodysystem([1.0, 1.0, 1.0]u"Msun", a=[0.1, 1.0]u"AU", e=[0.4, 0.2])
+        res = simulate(triple, t_sim=10, save_everystep=false, showprogress=false, 
+                       callbacks=["collision", "escape", "rlof", "tidal_disruption"])
+        res = simulate(triple, t_sim=10, save_everystep=false, showprogress=false, 
+                       callbacks=["collision", "escape", "rlof", "tidal_disruption"],
+                       potential=[PureGravitationalPotential(), DynamicalTidalPotential(G=𝒢.val, n=4, γ=[1.5, 1.5, 1.5])])
+        sol = analyse_simulation(res)
+    end
+
 end
