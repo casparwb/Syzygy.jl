972b0f47653d5b4bea86bb8278ba71dab9adc116
Caspar William Bruenech <c.w.bruenech@pm.me>
2023-07-25 06:28:17 +0200

    updated stopping conditions

--- src/simulation/callbacks.jl
+++ src/simulation/callbacks.jl
@@ -22,6 +22,7 @@ function setup_callbacks(stopping_conditions, system, p, retcode, G, args; start
                 push!(cbs, callback_collision)
 
             elseif condition == "escape"
+                @assert system.n == 3 "Escape check callback is currently only available for triples."
                 function condition_escape(u, t, integrator)
                     (integrator.iter % 100) == 0  # check for escape every 100 iteration
                 end
@@ -83,15 +84,15 @@ function setup_callbacks(stopping_conditions, system, p, retcode, G, args; start
                 @assert system.n == 3 "Democratic interaction callback is currently only available for triples."
                 affect_democratic1!(integrator) = democratic_check_callback1!(integrator, retcode, system)
                 affect_democratic2!(integrator) = democratic_check_callback2!(integrator, retcode, system)
-                # affect_democratic3!(integrator) = democratic_check_callback3!(integrator, retcode, system)
+                affect_democratic3!(integrator) = democratic_check_callback3!(integrator, retcode, system)
                 condition_democratic(u, t, integrator) = true
                 callback_democratic1 = DiscreteCallback(condition_democratic, affect_democratic1!, save_positions=(false, false))
                 callback_democratic2 = DiscreteCallback(condition_democratic, affect_democratic2!, save_positions=(false, false))
-                # callback_democratic3 = DiscreteCallback(condition_democratic, affect_democratic3!, save_positions=(false, false))
+                callback_democratic3 = DiscreteCallback(condition_democratic, affect_democratic3!, save_positions=(false, false))
 
                 push!(cbs, callback_democratic1)
                 push!(cbs, callback_democratic2)
-                # push!(cbs, callback_democratic3)
+                push!(cbs, callback_democratic3)
             
             else
                 continue
@@ -119,7 +120,7 @@ function collision_callback!(integrator, n, retcode)
                 d = norm(ri - rj)
 
                 if (d - integrator.p.R[j]) < integrator.p.R[i]
-                    t = u"kyr"(integrator.t * u"s")
+                    t = integrator.t * upreferred(1.0u"s")
                     retcode[:Collision] = ([i, j], t)
                     terminate!(integrator)
                 end
@@ -164,50 +165,56 @@ end
 
 
 """
-
 Returns a callback for checking if system has become unbound.
 """
 function unbound_callback!(integrator, retcode, system; max_a=100, G=upreferred(𝒢).val)
-    # positions = u.x[2]
-    # velocities = u.x[1]
-    # criteria = @MArray zeros(Bool, 4, system.n)
+
     u = integrator.u
-    @inbounds for i ∈ 1:system.n#particle ∈ system.particles
-        particle = system.particles[i]
+    combinations = SA[(1, SA[2, 3]), (2, SA[1, 3]), (3, SA[1, 2])]
+    @inbounds for (particle, binary_ids) in combinations
         
-        position = SA[u.x[2][1,i], u.x[2][2,i], u.x[2][3,i]]
-        velocity = SA[u.x[1][1,i], u.x[1][2,i], u.x[1][3,i]]
+        # escape candidate
+        r_part = SA[u.x[2][1,particle], u.x[2][2,particle], u.x[2][3,particle]]
+        v_part = SA[u.x[1][1,particle], u.x[1][2,particle], u.x[1][3,particle]]
 
-        sibling = particle.sibling
-        sibling_ids = get_particle_ids(system[sibling])
+        # remaining components
+        r_comp1 = SA[u.x[2][1,binary_ids[1]], u.x[2][2,binary_ids[1]], u.x[2][3,binary_ids[1]]]
+        r_comp2 = SA[u.x[2][1,binary_ids[2]], u.x[2][2,binary_ids[2]], u.x[2][3,binary_ids[2]]]
 
-        # r = get_state_vectors(u.x[2], u.x[1], integrator.p.M, system[sibling], sibling_ids)[1]
-        r = get_positions(u.x[2], integrator.p.M, system[sibling], sibling_ids)
-        r_rel = position  - r#, velocity  - v
+        v_comp1 = SA[u.x[1][1,binary_ids[1]], u.x[1][2,binary_ids[1]], u.x[1][3,binary_ids[1]]]
+        v_comp2 = SA[u.x[1][1,binary_ids[2]], u.x[1][2,binary_ids[2]], u.x[1][3,binary_ids[2]]]
+
+        r_rel_bin = r_comp1 - r_comp2
+        v_rel_bin = v_comp1 - v_comp2
+
+        # binary properties of remaining components
+        M_bin = SA[integrator.p.M[binary_ids[1]], integrator.p.M[binary_ids[2]]]
+        a_bin = semi_major_axis(norm(r_rel_bin), norm(v_rel_bin)^2, M_bin[1] + M_bin[2], G)
+
+        r_bin = centre_of_mass(SA[r_comp1, r_comp2], M_bin)
+
+        r_rel = r_part - r_bin
 
         d = norm(r_rel)
 
-        new_position = position + velocity .* integrator.dt
+        new_position = r_part + v_part .* integrator.dt
 
-        criteria_1 = d > (max_a * integrator.p.a[i])    # Body is a certain distance from sibling's centre of mass
-        criteria_2 = norm(new_position - r) > d       # Body is moving away from centre of mass
+        criteria_1 = d > (max_a * a_bin)                 # Body is a certain distance from sibling's centre of mass
+        criteria_2 = norm(new_position - r_bin) > d      # Body is moving away from centre of mass
         
         escape = criteria_1 && criteria_2
         if escape
-            escapee = i
-            T = kinetic_energy(velocity, integrator.p.M[i]) 
+            escapee = particle
+            M = integrator.p.M[particle]
+            T = kinetic_energy(v_part, M) 
             
-            U = -integrator.p.M[i]*integrator.p.M[sibling_ids[1]]/(norm(position - u.x[2][:,sibling_ids[1]]))
-            @inbounds for j in sibling_ids[2:end]
-                U -= integrator.p.M[i]*integrator.p.M[j]/(norm(position - u.x[2][:,j]))
-            end
-            U *= G
+            U = -(G*M)*(M_bin[1]/norm(r_part - r_comp1) + M_bin[2]/norm(r_part - r_comp2))
             
             Etot = T + U
             if Etot > zero(Etot)
                 retcode[:Escape] = escapee
                 terminate!(integrator)
-            elseif d >= upreferred(1.0u"pc").val      # Body is 1 parsec away from its sibling
+            elseif d >= upreferred(1.0u"pc").val      # Body is 1 parsec away from binary
                 retcode[:Drifter] = escapee
                 terminate!(integrator)
             end
@@ -219,14 +226,74 @@ end
 
 do_nothing!(integrator) = nothing
 
+# function rlof_callback!(integrator, retcode, system, G=upreferred(𝒢).val)
+
+#     u = integrator.u
+#     @inbounds for i ∈ 1:system.n
+#         rcode = Symbol("RLOF_$i")
+#         haskey(retcode, rcode) && continue
+        
+#         if !(stellar_type_from_index(integrator.p.stellar_type[i]) isa Star)
+#             continue
+#         end
+        
+#         particle = system.particles[i]
+
+#         position = SA[u.x[2][1,i], u.x[2][2,i], u.x[2][3,i]]
+#         velocity = SA[u.x[1][1,i], u.x[1][2,i], u.x[1][3,i]]
+
+#         sibling = particle.sibling
+#         siblings = system[sibling]
+#         sibling_ids = get_particle_ids(siblings)
+        
+#         r, v = get_state_vectors(u.x[2], u.x[1], integrator.p.M, siblings, sibling_ids)
+
+#         # m = integrator.p.M[sibling_ids]
+#         M₁ = integrator.p.M[i]
+#         M₂ = zero(M₁)
+
+#         for k in integrator.p.M[sibling_ids]
+#             M₂ += k
+#         end
+#         # M₂ = sum(m)
+#         r_rel = position - r
+#         v_rel = velocity - v
+
+#         d = norm(r_rel)
+#         # v² = norm(v_rel)^2
+
+#         # a = semi_major_axis(d, v², M₁ + M₂, G)
+#         # e = eccentricity(r_rel, v_rel, a, M₁ + M₂, G)
+
+#         R_roche = roche_radius(d, M₁/M₂)#*(1 - e)
+
+#         rlof = R_roche <= integrator.p.R[i]
+#         if rlof
+#             retcode[rcode] = (upreferred(1.0u"s")*integrator.t)
+#         end
+#     end
+# end
+
 function rlof_callback!(integrator, retcode, system, G=upreferred(𝒢).val)
 
+    if haskey(retcode, :Democratic_dist) || haskey(retcode, :Democratic_ecc) || haskey(retcode, :Democratic_sma)
+        rlof_callback_democratic!(integrator, retcode, system, G)
+    else
+        rlof_callback_hierarchical!(integrator, retcode, system, G)
+    end # end if
+end
+
+function rlof_callback_hierarchical!(integrator, retcode, system, G=upreferred(𝒢).val, particles=SA[(1:system.n)...])
     u = integrator.u
-    @inbounds for i ∈ 1:system.n#particle ∈ system.particles
-        particle = system.particles[i]
-        # particle = system.particles[i]
+    @inbounds for i ∈ 1:system.n
+        rcode = Symbol("RLOF_$i")
+        haskey(retcode, rcode) && continue
         
-        # position, velocity = u.x[2][:,i], u.x[1][:,i]
+        if !(stellar_type_from_index(integrator.p.stellar_type[i]) isa Star)
+            continue
+        end
+        
+        particle = system.particles[i]
 
         position = SA[u.x[2][1,i], u.x[2][2,i], u.x[2][3,i]]
         velocity = SA[u.x[1][1,i], u.x[1][2,i], u.x[1][3,i]]
@@ -244,6 +311,7 @@ function rlof_callback!(integrator, retcode, system, G=upreferred(𝒢).val)
         for k in integrator.p.M[sibling_ids]
             M₂ += k
         end
+
         # M₂ = sum(m)
         r_rel = position - r
         v_rel = velocity - v
@@ -254,18 +322,57 @@ function rlof_callback!(integrator, retcode, system, G=upreferred(𝒢).val)
         a = semi_major_axis(d, v², M₁ + M₂, G)
         e = eccentricity(r_rel, v_rel, a, M₁ + M₂, G)
 
-        R_roche = roche_radius(a, M₁/M₂)*(1 - e)
+        R_roche = roche_radius(d, M₁/M₂)#*(1 - e)
 
         rlof = R_roche <= integrator.p.R[i]
         if rlof
-            rcode = Symbol("RLOF_$i")
-            if !(rcode in keys(retcode))
-                retcode[rcode] = (upreferred(1.0u"s")*integrator.t)
-            end
-            # outcome(integrator)
+            retcode[rcode] = (upreferred(1.0u"s")*integrator.t)
         end
     end
 end
+
+function rlof_callback_democratic!(integrator, retcode, system, G=upreferred(𝒢).val, particles=SA[(1:system.n)...])
+    u = integrator.u
+    @inbounds for i ∈ particles
+        rcode = Symbol("RLOF_$i")
+        haskey(retcode, rcode) && continue
+
+        if !(stellar_type_from_index(integrator.p.stellar_type[i]) isa Star)
+            continue
+        end
+
+        position = SA[u.x[2][1,i], u.x[2][2,i], u.x[2][3,i]]
+        velocity = SA[u.x[1][1,i], u.x[1][2,i], u.x[1][3,i]]
+
+        sibling_ids = [part for part in particles if part != i]
+        distances = [norm(position - SA[u.x[2][1,j], u.x[2][2,j], u.x[2][3,j]]) for j in sibling_ids]
+        sibling = sibling_ids[argmin(distances)]
+        
+        r = SA[u.x[2][1,sibling], u.x[2][2,sibling], u.x[2][3,sibling]]
+        v = SA[u.x[1][1,sibling], u.x[1][2,sibling], u.x[1][3,sibling]]
+        
+        M₁ = integrator.p.M[i]
+        M₂ = integrator.p.M[sibling]
+
+        r_rel = position - r
+        v_rel = velocity - v
+
+        d = norm(r_rel)
+        # v² = norm(v_rel)^2
+
+        # a = semi_major_axis(d, v², M₁ + M₂, G)
+        # e = eccentricity(r_rel, v_rel, a, M₁ + M₂, G)
+
+        R_roche = roche_radius(d, M₁/M₂)#*(1 - e)
+
+        rlof = R_roche <= integrator.p.R[i]
+        if rlof
+            retcode[rcode] = (upreferred(1.0u"s")*integrator.t)
+        end
+    end
+
+end
+
 
 function tidal_disruption_callback!(integrator, retcode, system, G=upreferred(𝒢).val)
 
@@ -278,13 +385,13 @@ function tidal_disruption_callback!(integrator, retcode, system, G=upreferred(�
         end
         ri = @SVector [integrator.u.x[2][1, i], integrator.u.x[2][2, i], integrator.u.x[2][3, i]]
         for j ∈ i:system.n
-            if i != j && system.particles[j].structure.type isa BlackHole
+            if system.particles[j].structure.type isa BlackHole && i != j
                 rj = @SVector [integrator.u.x[2][1, j], integrator.u.x[2][2, j], integrator.u.x[2][3, j]]
                 d = norm(ri - rj)
 
                 tidal_disruption_radius = integrator.p.R[i]*cbrt(integrator.p.M[j]/integrator.p.M[i])
                 if (d - integrator.p.R[i]) < tidal_disruption_radius
-                    t = u"kyr"(integrator.t * u"s")
+                    t = integrator.t * upreferred(1.0u"s")
                     retcode[:TidalDisruption] = ([i, j], t)
                     terminate!(integrator)
                 end
@@ -353,11 +460,63 @@ function democratic_check_callback1!(integrator, retcode, system)
 end
 
 """
-Check if the original inner binary is no longer the one with the smallest semi-major axis,
-or if any of the binaries have hyperbolic orbits (e > 1).
+Check if the original inner binary is no longer the one with the smallest semi-major axis.
 """
 function democratic_check_callback2!(integrator, retcode, system)
-    if haskey(retcode, :Democratic_sma) && haskey(retcode, :Democratic_ecc) # only need to raise flags once
+    if haskey(retcode, :Democratic_sma) # only need to raise flags once
+        return
+    end
+
+    pairs = SA[(1,2), (2,3), (1,3)]
+    sma = Inf
+    bound_pair = (1, 2)
+    @inbounds for pair in pairs
+        i, j = pair
+
+        ri = SA[integrator.u.x[2][1, i], integrator.u.x[2][2, i], integrator.u.x[2][3, i]]
+        rj = SA[integrator.u.x[2][1, j], integrator.u.x[2][2, j], integrator.u.x[2][3, j]]
+
+        vi = SA[integrator.u.x[1][1, i], integrator.u.x[1][2, i], integrator.u.x[1][3, i]]
+        vj = SA[integrator.u.x[1][1, j], integrator.u.x[1][2, j], integrator.u.x[1][3, j]]
+
+        Mi, Mj = integrator.p.M[i], integrator.p.M[j]
+        M = Mi + Mj
+
+        r_rel = rj - ri
+        v_rel = vj - vi
+
+        vcom = centre_of_mass_velocity(SA[vi, vj], SA[Mi, Mj])
+        
+        K = 0.5*(Mi*norm(vi - vcom)^2 + Mj*norm(vj - vcom)^2)
+        U = -upreferred(𝒢.val)*Mi*Mj/norm(r_rel)
+    
+        (K + U) > 0 && continue # if not bound, skip
+
+        d = norm(r_rel)
+        v² = norm(v_rel)^2
+    
+        a = semi_major_axis(d, v², M, upreferred(𝒢.val))
+        a < zero(a) && continue
+
+        if a < sma
+            sma = a
+            bound_pair = pair
+        end
+
+    end
+
+    if bound_pair != (1, 2)
+        retcode[:Democratic_sma] = (true, integrator.t)
+    end
+
+
+end
+
+"""
+Check if any of the binaries have hyperbolic orbits (e > 1).
+"""
+function democratic_check_callback3!(integrator, retcode, system)
+    if haskey(retcode, :Democratic_ecc) # only need to raise flags once
         return
     end
 
@@ -365,45 +524,24 @@ function democratic_check_callback2!(integrator, retcode, system)
 
     r1 = SA[u.x[2][1,1], u.x[2][2,1], u.x[2][3,1]]
     r2 = SA[u.x[2][1,2], u.x[2][2,2], u.x[2][3,2]]
-    r3 = SA[u.x[2][1,3], u.x[2][2,3], u.x[2][3,3]]
 
     v1 = SA[u.x[1][1,1], u.x[1][2,1], u.x[1][3,1]]
     v2 = SA[u.x[1][1,2], u.x[1][2,2], u.x[1][3,2]]
-    v3 = SA[u.x[1][1,3], u.x[1][2,3], u.x[1][3,3]]
 
-    M1, M2, M3 = integrator.p.M[1], integrator.p.M[2], integrator.p.M[3]
+    M1, M2 = integrator.p.M[1], integrator.p.M[2]
     M12 = M1 + M2
-    M123 = M12 + M3
 
-    inner_com = centre_of_mass(SA[r1, r2], SA[M1, M2])
-    inner_com_vel = centre_of_mass_velocity(SA[v1, v2], SA[M1, M2])
+    r_rel = r2 - r1
+    v_rel = v2 - v1
 
-    r_3_inner_rel = r3 - inner_com
-    v_3_inner_rel = v3 - inner_com_vel
+    d = norm(r_rel)
+    v² = norm(v_rel)^2
 
-    r_12_rel = r2 - r1
-    v_12_rel = v2 - v1
-
-    d_3_inner = norm(r_3_inner_rel)
-    v²_3_inner = norm(v_3_inner_rel)^2
-
-    d_12 = norm(v_12_rel)
-    v²_12_rel = norm(v_12_rel)^2
-
-    a_12 = semi_major_axis(d_12, v²_12_rel, M12, upreferred(𝒢.val))
-    a_3_inner = semi_major_axis(d_3_inner, v²_3_inner, M123, upreferred(𝒢).val)
+    a = semi_major_axis(d, v², M12, upreferred(𝒢.val))
     
-    e_in = eccentricity(r_12_rel, v_12_rel, a_12, M12, upreferred(𝒢).val)
-    e_out = eccentricity(r_3_inner_rel, v_3_inner_rel, a_3_inner, M123, upreferred(𝒢).val)
-
-    if a_12 > a_3_inner
-        if !(haskey(retcode, :Democratic_sma))
-            retcode[:Democratic_sma] = (true, integrator.t)
-        end
-    elseif e_in >= 1 || e_out >= 1
-        if !(haskey(retcode, :Democratic_ecc))
-            retcode[:Democratic_ecc] = (true, integrator.t)
-        end
+    e = eccentricity(r_rel, v_rel, a, M12, upreferred(𝒢).val)
+    if e >= 1
+        retcode[:Democratic_ecc] = (true, integrator.t)
     end
 
 end
