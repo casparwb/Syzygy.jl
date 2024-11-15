9fa15bb14919ea3d108661b5c11df6d9f1e0e88a
Caspar William Bruenech <c.w.bruenech@proton.me>
2024-11-15 14:29:22 +0100

    another attempt at allowing customizable unit system with precompilation

--- src/Syzygy.jl
+++ src/Syzygy.jl
@@ -1,13 +1,44 @@
-__precompile__(false)
+# __precompile__(false)
 
 module Syzygy
-    using PrecompileTools, Reexport 
+    using PrecompileTools, Reexport, Preferences
     @reexport using Unitful, UnitfulAstro
 
-    Unitful.preferunits(u"Rsun, Msun, yr"...)
-    Unitful.register(Syzygy)
-#     function __init__()
-#     end
+    function set_units(units)
+        allowed_units = ["SI", "Solar", "CGS"]
+
+        if !(units in allowed_units)
+            throw(ArgumentError("Given unit system not allowed. Currently $(allowed_units) are allowed.")) 
+        end
+
+        @set_preferences!("units" => units)
+        @info "New default units set; restart your Julia session for this change to take effect!"
+    end
+
+    function units_from_unit_system(unit_system)
+        allowed_units = ["SI", "Solar", "SI"]
+        units = if unit_system == "SI"
+            u"kg, m, s"
+        elseif unit_system == "CGS"
+            u"g, cm, s"
+        elseif unit_system == "Solar"
+            u"Rsun, Msun, yr"
+        else
+            throw(ArgumentError("Given unit system not allowed. Currently $(allowed_units) are allowed.")) 
+        end
+
+        return units
+    end
+
+    const unit_system = @load_preference("units", "Solar")
+    const units = units_from_unit_system(unit_system)
+
+    Unitful.preferunits(units...)
+    const localpromotion = copy(Unitful.promotion)
+    function __init__()
+        Unitful.register(Syzygy)
+        merge!(Unitful.promotion, localpromotion)
+    end
 
 
     # include("units.jl")
@@ -32,14 +63,18 @@ module Syzygy
     include("analysis/visualization.jl")
     include("io.jl")
 
-    public centre_of_mass, centre_of_mass_velocity, potential_energy, 
+    public centre_of_mass, center_of_mass, centre_of_mass_velocity, potential_energy, 
            kinetic_energy, specific_orbital_energy, reduced_mass, gravitational_radius,
            schwarzschild_radius, roche_radius, octupole_parameter, is_unstable,
-           stability_criterion_ma01
+           stability_criterion_ma01, eccentricity
 
     export multibodysystem
     export PureGravitationalPotential, DynamicalTidalPotential, 
-           EquilibriumTidalPotential, StaticEquilibriumTidalPotential
+           EquilibriumTidalPotential, StaticEquilibriumTidalPotential,
+           PN1Potential, PN2Potential, PN2p5Potential, PNPotential,
+           PN1SpinPrecessionPotential, PN1p5SpinPrecessionPotential, PN2SpinPrecessionPotential,
+           SpinPrecessionPotential
+
     export simulation, simulate
     export to_solution
     export GRAVCONST
