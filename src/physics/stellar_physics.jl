479e1f885a2f9e3ede71bff2ce7763de5ce3952d
Caspar William Bruenech <c.w.bruenech@proton.me>
2025-04-30 13:34:13 +0200

    updated stellar physics to take stellar type numbers

--- src/physics/stellar_physics.jl
+++ src/physics/stellar_physics.jl
@@ -12,7 +12,6 @@ function envelope_structure(mass::Unitful.Mass, radius, core_mass, core_radius, 
     radius = ustrip(u"Rsun", radius)
     core_mass = ustrip(u"Msun", core_mass)
     core_radius = ustrip(u"Rsun", core_radius)
-    # stellar_type = ustrip(u"stp", stellar_type)
     age = ustrip(u"Myr", age)
 
     R_env, M_env = envelope_structure(mass, radius, core_mass, core_radius, stellar_type, age, Z)
@@ -25,7 +24,7 @@ function envelope_structure(star::Particle, age, Z=0.02)
 
     envelope_structure(star.structure.m, star.structure.R, 
                        star.structure.m_core, star.structure.R_core, 
-                       star.structure.stellar_type, age)
+                       star.structure.stellar_type.number, age)
 end
 
 
@@ -112,10 +111,10 @@ Quantities must be in units of solar mass and solar radii.
 Reference Hurley et al. 2002 - DOI: 10.1046/j.1365-8711.2002.05038.x
 """
 function convective_envelope_radius(mass, radius, core_radius, stellar_type, age, tMS, tBGB)
-    stellar_type_number = stellar_type.number
-    if any(stellar_type_number .== (3, 5, 6, 8, 9)) # giant-like stars
+    # stellar_type_number = stellar_type.number
+    if any(stellar_type .== (3, 5, 6, 8, 9)) # giant-like stars
         return radius - core_radius
-    elseif any(stellar_type_number .== (1, 7))   # main sequence stars
+    elseif any(stellar_type .== (1, 7))   # main sequence stars
         τ = age/tMS 
         
         R_env₀ = if mass > 1.25
@@ -130,7 +129,7 @@ function convective_envelope_radius(mass, radius, core_radius, stellar_type, age
                 end
 
         return R_env₀*(1 - τ)^0.25
-    elseif any(stellar_type_number .== (2, 8)) # Hertzsprung gap stars
+    elseif any(stellar_type .== (2, 8)) # Hertzsprung gap stars
         τ = (age - tMS)/(tBGB - tMS)
         return sqrt(τ)*(radius - core_radius)
     end
@@ -146,9 +145,9 @@ Quantities must be in units of solar mass and solar radii.
 Reference Hurley et al. 2000 - https://ui.adsabs.harvard.edu/abs/1981A&A....99..126H
 """
 function convective_envelope_mass(mass, core_mass, stellar_type, age, tMS, tBGB)
-    @assert stellar_type isa Star "Only stars have envelopes."
+    @assert stellar_type < 10 "Only stars have envelopes."
 
-    if any(stellar_type.number .== (1, 7)) 
+    if any(stellar_type .== (1, 7)) 
         M_env₀ = if mass < 0.35
                      mass
                  elseif mass > 1.25
@@ -159,7 +158,7 @@ function convective_envelope_mass(mass, core_mass, stellar_type, age, tMS, tBGB)
         
         τ = age/tMS
         return M_env₀*(1 - τ)^0.25
-    elseif any(stellar_type.number .== (2, 8))
+    elseif any(stellar_type .== (2, 8))
         τ = (age - tMS)/(tBGB - tMS)
         return τ*(mass - core_mass)
     else 
